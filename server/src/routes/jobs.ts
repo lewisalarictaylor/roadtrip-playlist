97b08eb572855376e75d77c0b3f3cb8d81cba267
Lewis <lewistaylor43@googlemail.com>
2026-03-26 13:58:55 +0000

    still wrangling

--- server/src/routes/jobs.ts
+++ server/src/routes/jobs.ts
@@ -2,7 +2,7 @@ import type { FastifyPluginAsync } from 'fastify'
 import { query } from '../db/client.js'
 import { playlistQueue } from '../jobs/queue.js'
 import type { JobSettings } from '../../../shared/types'
-import { DEFAULT_SETTINGS } from '../../../shared/types'
+// import { DEFAULT_SETTINGS } from '../../../shared/types'
 
 export const jobRoutes: FastifyPluginAsync = async (fastify) => {
 
